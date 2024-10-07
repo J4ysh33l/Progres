2cfc506cfb7e7383c0b0c967a4ff7a1490d2107e
Jaysheel <jaysheelbhatt47@gmail.com>
2024-10-07 15:42:38 +0530

    delted few items

--- algoTB/lcm.py
+++
@@ -1,9 +0,0 @@
-from math import gcd
-
-def lcm(a, b):
-    """Calculate the lowest common multiple of 2 int's: a, b using `gcd()`."""
-    return a * b // gcd(a, b)
-
-if __name__ == '__main__':
-    a, b = map(int, input().split())
-    print(lcm(a, b))
